2092fe7be9e329d31d1e01b086e1947417b53a99
JingTao Chen <chenjingtao930@gmail.com>
2018-12-07 16:32:12 +0800

    Args checking functions Complete

--- test/doctest/run_doctest.py
+++ test/doctest/run_doctest.py
@@ -4,5 +4,6 @@ os.chdir(DIR)
 
 import doctest, glob
 for i in glob.glob(DIR+"/*.txt"):
-    print "TESTING\t", i
-    doctest.testfile(i)
+    print "TESTING\t", i, 
+    FAIL, TESTED = doctest.testfile(i, optionflags=doctest.ELLIPSIS)
+    print "........ TEST: %3d | FAIL: %3d " % (TESTED, FAIL)
